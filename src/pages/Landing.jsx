35c2300b99eb285dcf49d65956a0fbd8f9e9665d
Ucheizzzy <uchechukwuobiakor@gmail.com>
2023-08-13 23:23:08 +0100

    creating pages

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,27 @@
+import joblane from '../assets/images/joblane.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components'
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h1>
+            Job <span>Tracking</span> App
+          </h1>
+          <p>
+            Track your job application and never loose sight of it. Keep it and
+            update it anytime, your job application deserves a good treatment..
+          </p>
+          <button className='btn btn-orange'>Login/Regoster</button>
+        </div>
+        <img src={joblane} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  )
+}
+
+export default Landing
